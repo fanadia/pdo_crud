b7c997487ff097b81500cc1e1215097291c74f78
fanadia <fanadia.s@yru.ac.th>
2017-05-22 01:51:46 -0700

    Dbครั้งที่2

--- libs/Db.php
+++ libs/Db.php
@@ -1,24 +1,27 @@
-<?php class Db {
+<?php
+class Db {
   public $database;
   public function __construct(){
-    $this->connect();
+   $this->connect();
   }
-  
   public function __destruct(){
-    $this->disconnect();
+   $this->disconnect();
   }
- private function connect(){
+  private function connect(){
     $db_host = "localhost";
-    $db_name = "phprookie_db";
-$db_user = "root";
+    $db_name = "phprookie_db";$db_user = "root";
     $db_pass = "";
-
     try {
-        $this->database = new PDO("mysql:host=$db_host; dbname=$db_name", $db_user, $db_pass);
-        $this->database->exec("SET CHARACTER SET utf8");
+    $this->database = new PDO("mysql:host=$db_host;
+    dbname=$db_name", $db_user, $db_pass);
+    $this->database->exec("SET CHARACTER SET
+    utf8");
     }
     catch (PDOException $e) {
-        echo $e->getMessage();
+      echo $e->getMessage();
     }
   }
-     ?>
+  private function disconnect(){
+    $this->database = null;
+  }
+}
